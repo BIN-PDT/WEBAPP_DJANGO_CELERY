07f5a4b29bcf07798ee8f7ff13d9bd7108482585
BIN BIN <thangpd300502@gmail.com>
2025-06-03 21:47:36 +0700

    SETUP FUNCTIONALITY

--- a_home/urls.py
+++ a_home/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.home_view, name="home"),
+    path("unsubscribe/<int:id>", views.delete_subscriber, name="unsubscribe"),
 ]
--- a_home/views.py
+++ a_home/views.py
@@ -1,6 +1,44 @@
 from django.http import HttpRequest
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import redirect, render, get_object_or_404
+from .models import Message, Subscriber
+from .forms import MessageCreateForm, SubscriberCreateForm
 
 
 def home_view(request: HttpRequest):
-    return render(request, "a_home/home.html")
+    message_data = Message.objects.all()
+    message_form = MessageCreateForm()
+    subscriber_data = Subscriber.objects.all()
+    subscriber_form = SubscriberCreateForm()
+
+    if request.method == "POST":
+        is_message = request.POST.get("content") != None
+        if is_message:
+            form = MessageCreateForm(request.POST)
+            if form.is_valid():
+                form.save()
+            else:
+                messages.error(request, "Opps! Something went wrong.")
+        else:
+            form = SubscriberCreateForm(request.POST)
+            if form.is_valid():
+                form.save()
+            else:
+                messages.error(request, "Opps! Something went wrong.")
+
+        return redirect("home")
+
+    context = {
+        "message_data": message_data,
+        "message_form": message_form,
+        "subscriber_data": subscriber_data,
+        "subscriber_form": subscriber_form,
+    }
+    return render(request, "a_home/home.html", context)
+
+
+def delete_subscriber(_: HttpRequest, id: int):
+    subscriber = get_object_or_404(Subscriber, pk=id)
+    subscriber.delete()
+
+    return redirect("home")
